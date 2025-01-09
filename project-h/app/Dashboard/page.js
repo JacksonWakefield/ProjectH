d700ab325eeb0f2c5481fa8d0392f9a86a8d6d8d
JacksonWakefield <55509800+JacksonWakefield@users.noreply.github.com>
2025-01-09 02:31:47 -0700

    Beginning of Dashboard + test components

---
+++ project-h/app/Dashboard/page.js
@@ -0,0 +1,11 @@
+import DashboardCard from "@/components/page/Dashboard/DashboardCard";
+
+export default function Dashboard(){
+    
+
+    return (
+        <div className="DashboardWrapper">
+            <DashboardCard></DashboardCard>
+        </div>
+    );
+}
---
+++ project-h/components/page/Dashboard/DashboardCard.js
@@ -0,0 +1,10 @@
+
+
+export default function DashboardCard(){
+    
+
+
+    return (
+        <p>This is a dashboard card</p>
+    )
+}
